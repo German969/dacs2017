fb61ca48a10f90ff6f723fbde71b56bd36072c7e
German969 <german.monzon.95@gmail.com>
2017-04-28 17:15:39 -0300

    clase insertUsuario
    Falta completar las clases usuario y pedido con lo que nos pidio el
    profe

---
+++ primerEntrega/src/main/java/pruebas/insertUsuario.java
@@ -0,0 +1,41 @@
+package pruebas;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+//import javax.persistence.PersistenceContext;
+import primerEntrega.Usuario;
+
+public class insertUsuario {
+	
+//	@PersistenceContext(unitName="com.persistencia") 
+//	private static EntityManager entityManager;
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.persistencia");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Usuario u1 = new Usuario();
+		Usuario u2 = new Usuario();
+		Usuario u3 = new Usuario();
+		
+		u1.setDni("12345678");
+		u1.setNombre("Jorge");
+		
+		u2.setDni("87654321");
+		u2.setNombre("Pedro");
+		
+		u3.setDni("11223344");
+		u3.setNombre("Ana");
+		
+		entityManager.persist(u1);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		
+	}
+}
